8fb6fe7eb56de0b19ea4ab30bb9ecd3145365063
jpyan <jpyan@umich.edu>
2019-01-14 22:46:21 -0500

    try

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { TopBar } from './TopBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <TopBar/>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
